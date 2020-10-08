cad33fb2afe797071e0e074bd32a6cdfd8045109
oatsu <panchi.psp@gmail.com>
2020-10-09 02:57:11 +0900

    Update make_it_for_release.py
    release内にフォルダを作るようにした。

--- 00-svs-world/make_it_for_release.py
+++ 00-svs-world/make_it_for_release.py
@@ -12,54 +12,55 @@ from shutil import copy2
 from tqdm import tqdm
 
 SINGER = 'oniku_kurumi'
-PATH_QUESTION = 'conf/jp_qst001_nnsvs_simple_4_LLRR.hed'
-NAME_EXPERIMENT = 'jp_qst001_nnsvs_simple_4_LLRR'
+RELEASE_DIR = 'release/onikuru_---'
+PATH_QUESTION = 'conf/jp_qst001_nnsvs_simple_4_LL.hed'
+NAME_EXPERIMENT = 'jp_qst001_nnsvs_simple_4_LL'
 
 
-def copy_question(path_question):
+def copy_question(path_question, release_dir):
     """
     hedファイル(question)をコピー
     """
-    makedirs('release/conf', exist_ok=True)
+    makedirs(f'{release_dir}/conf', exist_ok=True)
     print('copying question')
-    copy2(path_question, f'release/{path_question}')
+    copy2(path_question, f'{release_dir}/{path_question}')
 
 
-def copy_scaler(singer):
+def copy_scaler(singer, release_dir):
     """
     dumpフォルダにあるファイルをコピー
     """
-    makedirs(f'release/dump/{singer}/norm', exist_ok=True)
+    makedirs(f'{release_dir}/dump/{singer}/norm', exist_ok=True)
     list_path_scaler = glob(f'dump/{singer}/norm/*_scaler.joblib')
 
     print('copying scaler')
     for path_scaler in tqdm(list_path_scaler):
-        copy2(path_scaler, f'release/{path_scaler}')
+        copy2(path_scaler, f'{release_dir}/{path_scaler}')
 
 
-def copy_model(singer, name_exp):
+def copy_model(singer, name_exp, release_dir):
     """
     name_exp: 試験のID
     """
     name_exp = singer + '_' + name_exp
-    makedirs(f'release/exp/{name_exp}/acoustic', exist_ok=True)
-    makedirs(f'release/exp/{name_exp}/duration', exist_ok=True)
-    makedirs(f'release/exp/{name_exp}/timelag', exist_ok=True)
+    makedirs(f'{release_dir}/exp/{name_exp}/acoustic', exist_ok=True)
+    makedirs(f'{release_dir}/exp/{name_exp}/duration', exist_ok=True)
+    makedirs(f'{release_dir}/exp/{name_exp}/timelag', exist_ok=True)
     list_path_model = glob(f'exp/{name_exp}/*/latest.pth')
     list_path_model += glob(f'exp/{name_exp}/*/model.yaml')
 
     print('copying model')
     for path_model in tqdm(list_path_model):
-        copy2(path_model, f'release/{path_model}')
+        copy2(path_model, f'{release_dir}/{path_model}')
 
 
 def main():
     """
     各種ファイルをコピーする
     """
-    copy_question(PATH_QUESTION)
-    copy_scaler(SINGER)
-    copy_model(SINGER, NAME_EXPERIMENT)
+    copy_question(PATH_QUESTION, RELEASE_DIR)
+    copy_scaler(SINGER, RELEASE_DIR)
+    copy_model(SINGER, NAME_EXPERIMENT, RELEASE_DIR)
 
 
 if __name__ == '__main__':
